6f395c12fc87ab74d8c75c78366e22b85e012411
steven07a <stevenherrera70396@gmail.com>
2022-11-18 23:36:11 -0800

    login page is added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,9 +5,14 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <>
-      <div>Home</div>
+      <h1>Home</h1>
       {/* shows how to link to different webpages refrences index.js to find the pages */}
-      <Link to={"/Test"}>go to test page</Link>
+      <div class="Links">
+        <Link to={"Login"}>Go to Login</Link>
+        <br></br>
+        <br></br>
+        <Link to={"Test"}>Go to test page</Link>
+      </div>
     </>
   );
 }
