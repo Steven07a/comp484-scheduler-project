3ada19b1dc11ca461adf622e163a178d4400ad41
Areitsu <luievtreyes@gmail.com>
2022-11-21 11:54:51 -0800

    Updated Login/Signup Page
    Changed Login/Signup Page.
    
    LoginPage folder was for tailwindcss but gave me issues.
    
    Will still try to use tailwind if not just regular CSS

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,69 +1,79 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import * as Components from "../components/Components"
-import "../components/styles.css";
-import { Link } from "react-router-dom";
+import React from 'react'
+import '../components/LoginForm.css'
 
-function Login() {
-  const [signIn, toggle] = React.useState(true);
-  return (
-    <Components.Container>
-      <Components.SignUpContainer signingIn={signIn}>
-        <Components.Form>
-          <Components.Title>Create Account</Components.Title>
-          <Components.Input type="text" placeholder="Name" />
-          <Components.Input type="email" placeholder="Email" />
-          <Components.Input type="password" placeholder="Password" />
-          <Components.Button>Sign Up</Components.Button>
-        </Components.Form>
-      </Components.SignUpContainer>
-      <Components.SignInContainer signingIn={signIn}>
-        <Components.Form>
-          <Components.Title>Login</Components.Title>
-          <Components.Input type="email" placeholder="Email" />
-          <Components.Input type="password" placeholder="Password" />
-          <Components.Anchor href="#">Forgot your password?</Components.Anchor>
-          <Components.Button onClick={"/Home"}>Sign In</Components.Button>
-        
-        </Components.Form>
-      </Components.SignInContainer>
-      <Components.OverlayContainer signingIn={signIn}>
-        <Components.Overlay signingIn={signIn}>
-          <Components.LeftOverlayPanel signingIn={signIn}>
-            <Components.Title2>Your Static is Waiting for you.</Components.Title2>
-            <Components.Paragraph>
-              Make sure you got everyone food and drinks.
-            </Components.Paragraph>
-            <Components.GhostButton onClick={() => toggle(true)}>
-              Log back in
-            </Components.GhostButton>
-          </Components.LeftOverlayPanel>
-          <Components.RightOverlayPanel signingIn={signIn}>
-            <Components.Title2>Raid Finder and Scheduler</Components.Title2>
-            <Components.Paragraph>
-            Looking for your next Deathless Clear?
-            </Components.Paragraph>
-            <Components.GhostButton onClick={() => toggle(false)}>
-              Sign Up now
-            </Components.GhostButton>
-          </Components.RightOverlayPanel>
-        </Components.Overlay>
-      </Components.OverlayContainer>
-    </Components.Container>
-  );
+class LoginForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            maskStyle: {
+                left: 512,
+                right: 0
+            }
+        }
+
+        this.container = React.createRef()
+
+        this.onSignIn = this.onSignIn.bind(this)
+        this.onSignUp = this.onSignUp.bind(this)
+
+    }
+    componentDidMount() {
+      
+    }
+    componentWillUnmount() {
+        clearInterval(this.interval);
+
+    }
+
+    onSignUp() {
+        this.container.current.classList.add("right-panel-active");
+    }
+    onSignIn() {
+        debugger
+        this.container.current.classList.remove("right-panel-active");
+    }
+
+    render() {
+        const clipValue = `inset(0 ${this.state.maskStyle.left}px 0 ${this.state.maskStyle.right}px)`
+        // const clipValue =  `inset(0 200px 0 200px)`
+
+        return (
+            <div className="container" id="container" ref={this.container}>
+                <div className="form-container sign-up-container">
+                    <form action="#">
+                        <h4>Create Account</h4>
+                        <input type="text" placeholder="Name" />
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <button onClick={this.onSignUp}>Sign Up</button>
+                    </form>
+                </div>
+                <div className="form-container sign-in-container">
+                    <form action="#">
+                        <h4>Sign in</h4>
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <a href="#">Forgot your password?</a>
+                        <button onClick={this.onSignIn}>Sign In</button>
+                    </form>
+                </div>
+                <div className="overlay-container">
+                    <div className="overlay">
+                        <div className="overlay-panel overlay-left">
+                            <h4>Your Static is Waiting for you.</h4>
+                            <p>Make sure you got everyone food and drinks.</p>
+                            <button className="ghost" id="signIn" onClick={this.onSignIn}>Log Back In</button>
+                        </div>
+                        <div className="overlay-panel overlay-right">
+                            <h4>Raid Finder and Scheduler</h4>
+                            <p>Looking for your next Deathless Clear?</p>
+                            <button className="ghost" id="signUp" onClick={this.onSignUp}>Sign Up Now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
-function Home() {
-    return (
-      <>
-        <div>Home</div>
-        {/* shows how to link to different webpages refrences index.js to find the pages */}
-        <Link to={"/Test"}>go to test page</Link>
-      </>
-    );
-  }
-<Link to={"/Test"}>go to test page</Link>
-const rootElement = document.getElementById("root");
-ReactDOM.render(<Login />, rootElement);
 
-
-export default Login;
+export default LoginForm;
---
+++ client/tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  mode: "jit",
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      zIndex: {
+        100: 100,
+      },
+
+      keyframes: {
+        show: {
+          "0%, 49.99%": { opacity: 0, "z-index": 10 },
+          "50%, 100%": { opacity: 1, "z-index": 50 },
+        },
+      },
+
+      animation: {
+        show: "show 0.7s",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
