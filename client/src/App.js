9c8e9015ea88a2358263dc79b8b8a3ef41f44822
Areitsu <luievtreyes@gmail.com>
2022-11-27 17:08:16 -0800

    added selection for time/days available

--- client/src/App.js
+++ client/src/App.js
@@ -61,7 +61,7 @@ const router = createBrowserRouter([
   //   element: <Register />,
   // },
   {
-      path: "/Profile",
+      path: "Profile",
       element: <Profile />
   },
 ]);
--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import RosterTable from "./RosterTable/RosterTable";
 import Input from "./Input/Input"
 import "./Dashboard.css";
+import "../../components/Navigation/Navbar.css"
+import { AuthContext, logout } from "../../context/authContext"
 import Button from '@mui/joy/Button';
 import Menu from '@mui/joy/Menu';
 import MenuItem from '@mui/joy/MenuItem';
@@ -9,6 +11,7 @@ import Apps from '@mui/icons-material/Apps';
 import { Link } from "react-router-dom";
 
 export default function SelectedMenu() {
+  const { currentUser, logout } = useContext(AuthContext);
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [selectedIndex, setSelectedIndex] = React.useState(1);
   const open = Boolean(anchorEl);
@@ -23,10 +26,29 @@ export default function SelectedMenu() {
   };
 
   return (
+    <>
+    <nav className="nav">
+        <a href="/">Home</a>
+        {currentUser?.email && (
+          <>
+            <br />
+            <button onClick={logout}>logout</button>
+          </>
+        )}
+        <ul>
+            <li>
+                <Link to={"Raids"}>Raid Finder</Link>
+            </li>
+            <li>
+                <Link>Profile</Link>
+            </li>
+        </ul>
+    </nav>
     <div className="Dashboard">
       <h1>Dashboard</h1>
       <Input />
       <RosterTable />
     </div>
+    </>
   );
 };
